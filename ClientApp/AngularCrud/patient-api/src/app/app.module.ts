0b1d8f87c3ac90b3c42578668ca07fd628c7dc5b
Afsana Sultana <Afsana Sultana@DESKTOP-QBFDN9A>
2021-12-06 18:01:14 +0600

    navbar.component.css navbar.component.html navbar.component.spec.ts navbar.component.ts app.component.ts app.component.html app.module.ts index.html style.css angular.json package-lock.json package.json

--- ClientApp/AngularCrud/patient-api/src/app/app.module.ts
+++ ClientApp/AngularCrud/patient-api/src/app/app.module.ts
@@ -2,15 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
