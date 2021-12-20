bd89576d56d6c9d74dcab9a36bdc5c66797a8b10
Afsana Sultana <Afsana Sultana@DESKTOP-QBFDN9A>
2021-12-20 18:03:31 +0600

    getappointment.component.html patientModel.cs

--- PatientAPI/Models/PatientModel.cs
+++ PatientAPI/Models/PatientModel.cs
@@ -13,21 +13,22 @@ namespace PatientAPI.Models
         public string Country { get; set; }
         public string City { get; set; }
         public string CountryTravelingTo { get; set; }
+        public string MedicalCenter { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public DateTime DateOfBirth { get; set; }
         public string Nationality { get; set; }
-        public string EmailId { get; set; }
-        public string PhoneNo { get; set; }
         public string Gender { get; set; }
         public string MaritalStatus { get; set; }
+        public string EmailId { get; set; }
+        public string PhoneNo { get; set; }
+        public string NationalId { get; set; }
         public string PassportNo { get; set; }
         public string ConfirmPassportNo { get; set; }
         public DateTime PassportIssueDate { get; set; }
         public string PassportIssuePlace { get; set; }
         public DateTime PassportExpiryDate { get; set; }
         public string VisaType { get; set; }
-        public string NationalId { get; set; }
         public string PositionAppliedFor { get; set; }
         public string Other { get; set; }
 
